52cd7dc2306fa99afd0d3d9f24515082a4d40321
zahramobasheramini <zhmamini@gmail.com>
2026-07-29 18:32:32 +0330

    Support multi-word text search

--- search_text.py
+++ search_text.py
@@ -1,9 +1,28 @@
+# with open("sample.txt", "r", encoding="utf-8") as file:
+#     text = file.read()
+
+# keyword = input("Enter a keyword: ")
+
+# if keyword.lower() in text.lower():
+#     print("The keyword was found.")
+# else:
+#     print("The keyword was not found.")
+
 with open("sample.txt", "r", encoding="utf-8") as file:
     text = file.read()
 
-keyword = input("Enter a keyword: ")
+question = input("Enter your question: ")
 
-if keyword.lower() in text.lower():
-    print("The keyword was found.")
+words = question.lower().split()
+text = text.lower()
+
+found_words = []
+
+for word in words:
+    if word in text:
+        found_words.append(word)
+
+if found_words:
+    print("Found words:", found_words)
 else:
-    print("The keyword was not found.")
+    print("No words were found.")
